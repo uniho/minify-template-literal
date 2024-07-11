d3828f837cf322fef38a0fd857bccc9808eb4e06
uniho <uniho@users.noreply.github.com>
2024-07-11 15:10:31 +0900

    Remove single line comments as well

--- src/plugin_emotion.js
+++ src/plugin_emotion.js
@@ -4,7 +4,7 @@ export default options => ({
   checkTag: (tag, node) => (tag == 'css' || tag == 'keyframes' || tag == 'styled()'),
   minify: src => src
     // remove comments
-    .replace(/(\/\*!.*?\*\/|\"(?:(?!(?<!\\)\").)*\"|\'(?:(?!(?<!\\)\').)*\')|\/\*[\s\S]*?\*\//g, '$1')
+    .replace(/(\/\*!.*?\*\/|\"(?:(?!(?<!\\)\").)*\"|\'(?:(?!(?<!\\)\').)*\')|\/\*[\s\S]*?\*\/|\/\/.*/g, '$1')
 
     // reduce whitespace to a single space
     .replace(/(\/\*!.*?\*\/|\"(?:(?!(?<!\\)\").)*\"|\'(?:(?!(?<!\\)\').)*\')\s*|\s+/g, '$1 ')
